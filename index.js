bba0a51ee76c021b7ca45d5dcaa9d7487752dfe9
Jean Felipe <JFBimCopinski@gmail.com>
2024-06-19 17:57:13 -0300

    feat: utilizei o prettier pra deixar mais bonito

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var input
-var numero1
-var numero2
+var input;
+var numero1;
+var numero2;
 
-console.log("Digite seu primeiro número.")
-process.stdin.on("data", function(data){
-    input = Number(data.toString().trim())
-    if (!numero1) {
-        numero1 = input
+console.log("Digite seu primeiro número.");
+process.stdin.on("data", function (data) {
+  input = Number(data.toString().trim());
+  if (!numero1) {
+    numero1 = input;
 
-        console.log("Digite seu segundo número.")
+    console.log("Digite seu segundo número.");
+  } else {
+    numero2 = input;
+    if (numero1 > numero2) {
+      i = numero1;
     } else {
-        numero2 = input
-        if (numero1>numero2) {
-        i = numero1
-        } else {
-            i = numero2
-        }
-
-        for (i; i <= numero1*numero2; i++){
-            if( i % numero1 == 0 && i % numero2 == 0) {
-                console.log("MMC: " + i)
-                break
-            }
-        }
+      i = numero2;
     }
-})
+
+    for (i; i <= numero1 * numero2; i++) {
+      if (i % numero1 == 0 && i % numero2 == 0) {
+        console.log("MMC: " + i);
+        break;
+      }
+    }
+  }
+});
